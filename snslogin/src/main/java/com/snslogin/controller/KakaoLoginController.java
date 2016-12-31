7a436770d8920047b90467b330dc864ad9d102cb
jbs <jbs@jbs-PC>
2017-01-01 07:36:04 +0900

    [update] [KakaoLoginController.java 리팩토링]

--- snslogin/src/main/java/com/snslogin/controller/KakaoLoginController.java
+++ snslogin/src/main/java/com/snslogin/controller/KakaoLoginController.java
@@ -33,28 +33,25 @@ public class KakaoLoginController {
 	// 애플리케이션 등록 후 발급받은 appKey [ 수정 O (카카오 홈페이지에 설정한 콜백 주소) ]
 	private final String appKey = "424b55f5ddb82448866d67cfe0a6aeec";
 	// 인증의 결과를 전달받을 콜백 URL ( 로그인 요청을 처리할때 콜백 URI 값과 합쳐진다) [수정 X ]
-	private String redirectUri = "";
+	private String redirectURL = "";
 	// 인증의 결과를 전달받을 콜백 URI . [ 수정 O (카카오 홈페이지에 설정한 콜백 주소) ]
-	private final String finalRedirectUri = "/redirecturikakao";
+	private final String RedirectURI = "/redirecturikakao";
 	// 로그인 요청에서 얻은 code(토큰)값 [수정 X ]
 	private String authorizeCode = "";
+	// 사용자 토큰 요청할 URL [수정 X ]
+	private final String tokenUrl = "https://kauth.kakao.com/oauth/token?"
+			+ "grant_type=authorization_code&client_id={appKey}&redirect_uri={redirectUri}&code={authorizeCode}";
+	// 요청할때 사용할 객체.
+	private RestTemplate restTemplate = new RestTemplate();
 
 	// 로인요청 성공후 콜백 되는 URL
 	@GetMapping("/redirecturikakao")
-	public String redirectUri(String code, HttpServletResponse response)
-			throws RestClientException, URISyntaxException {
+	public String redirectUri(String code, HttpServletResponse response) {
 		// 로그인 성공후 받은 code 값
 		authorizeCode = code;
 
-		// 요청할때 사용할 객체.
-		RestTemplate restTemplate = new RestTemplate();
-
-		// 사용자 토큰 요청할 URL
-		final String tokenUrl = "https://kauth.kakao.com/oauth/token?"
-				+ "grant_type=authorization_code&client_id={appKey}&redirect_uri={redirectUri}&code={authorizeCode}";
-
 		// 사용자 토큰 요청
-		Kakao.Token kakaoToken = restTemplate.getForObject(tokenUrl, Kakao.Token.class, appKey, redirectUri,
+		Kakao.Token kakaoToken = restTemplate.getForObject(tokenUrl, Kakao.Token.class, appKey, redirectURL,
 				authorizeCode);
 
 		// 발급 받은 사용자 토큰을 사용해서 카카오 정보 요청
@@ -71,24 +68,17 @@ public class KakaoLoginController {
 		return "redirect:/";
 	}
 
-	// 사용자 토큰 요청할 URL
-	private String requestTokenURL() {
-		return String.format(
-				"https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=%s&redirect_uri=%s&code=%s",
-				appKey, redirectUri, authorizeCode);
-	}
-
 	// 로그인 요청
 	@GetMapping("/kakaologin")
 	public String login(HttpServletRequest request) {
 		// 인증의 결과를 전달받을 콜백 URL 얻어 온다. ( 응답 데이터 : http://Host/redirecturikakao )
-		redirectUri = request.getRequestURL().toString().replace(request.getServletPath(), finalRedirectUri);
+		redirectURL = request.getRequestURL().toString().replace(request.getServletPath(), RedirectURI);
 
-		log.info("[Kakaologin] [로그인을 요청한 클라이언트 정보] [url : {}], [ip : {}]", redirectUri, request.getRemoteAddr());
+		log.info("[Kakaologin] [로그인을 요청한 클라이언트 정보] [url : {}], [ip : {}]", redirectURL, request.getRemoteAddr());
 
 		return String.format(
 				"redirect:https://kauth.kakao.com/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code",
-				appKey, redirectUri);
+				appKey, redirectURL);
 	}
 
 }
